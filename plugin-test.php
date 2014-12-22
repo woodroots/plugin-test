3ab98e15a6f8d0a3968c49db2b84789640984df3
woodroots <morimoto@wood-roots.com>
2014-12-22 21:10:09 +0900

    edit

--- plugin-test.php
+++ plugin-test.php
@@ -3,7 +3,6 @@
 Plugin Name: Plugin Test
 Plugin URI: https://github.com/woodroots/plugin-test
 Version: 1.0.1
-Text Domain: plugin-test
 GitHub Plugin URI: https://github.com/woodroots/plugin-test
 */
 
