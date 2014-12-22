fb737e43eb16b67f8fae5abceb9bd3d72bc5d73b
woodroots <morimoto@wood-roots.com>
2014-12-22 20:45:55 +0900

    edit

--- plugin-test.php
+++ plugin-test.php
@@ -4,7 +4,6 @@ Plugin Name: Plugin Test
 Plugin URI: https://github.com/woodroots/plugin-test
 Version: 0.2
 GitHub Plugin URI: https://github.com/woodroots/plugin-test
-GitHub Branch: master
 */
 
 function customize_footer($text) {
